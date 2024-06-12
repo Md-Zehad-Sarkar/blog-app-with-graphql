aafab2af481e3d27fe889bd6df2dd1620a8763ba
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2024-06-13 00:38:34 +0600

    user created using prisma and graphql

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,9 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema/schema"; 
+import { resolvers } from "./resolvers/resolver"; 
 
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
 
-  
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
 
 async function main() {
   const server = new ApolloServer({
